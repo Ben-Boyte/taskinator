e9cbafa8cc3194a315b6f94b54634e2efd30b51b
Ben-Boyte <zissette1@gmail.com>
2022-05-03 17:48:49 -0500

    added task type and info

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,21 @@ var createTaskHandler = function(event) {
 
     event.preventDefault();
   
-    var listItemEl = document.createElement("li");
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+   var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
     
-    console.log(event);
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    tasksToDoEl.appendChild(listItemEl);
+    console.dir(listItemEl);
   };
 
   formEl.addEventListener("submit", createTaskHandler);
